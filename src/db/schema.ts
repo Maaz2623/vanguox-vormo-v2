28639b35ca7ef4953ec67aa97ad85d536f2d3bc5
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2025-02-24 22:24:20 +0530

    modified:   bun.lock 	new file:   drizzle.config.ts 	modified:   package.json 	new file:   src/app/(auth)/layout.tsx 	new file:   src/app/(auth)/sign-in/[[...sign-in]]/page.tsx 	new file:   src/app/(auth)/sign-up/[[...sign-up]]/page.tsx 	new file:   src/app/api/trpc/[trpc]/route.ts 	new file:   src/app/api/users/webhook/route.ts 	modified:   src/app/layout.tsx 	new file:   src/db/index.ts 	new file:   src/db/schema.ts 	new file:   src/lib/ratelimit.ts 	new file:   src/lib/redis.ts 	modified:   src/middleware.ts 	new file:   src/trpc/client.tsx 	new file:   src/trpc/init.ts 	new file:   src/trpc/query-client.tsx 	new file:   src/trpc/routers/_app.ts 	new file:   src/trpc/server.tsx

---
+++ src/db/schema.ts
@@ -0,0 +1,21 @@
+import {
+  pgTable,
+  text,
+  timestamp,
+  uniqueIndex,
+  uuid,
+} from "drizzle-orm/pg-core";
+
+export const users = pgTable(
+  "users",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    email: text("email").unique().notNull(),
+    clerkId: text("clerk_id").unique().notNull(),
+    name: text("name").notNull(),
+    imageUrl: text("image_url").notNull(),
+    createdAt: timestamp("create_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  },
+  (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]
+);
